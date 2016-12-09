8fceec3612203188c23b7c0c3f9e1cbcd84a460f
shavit <shavit@users.noreply.github.com>
2016-12-09 00:24:08 -0500

    📚

--- test/index.js
+++ test/index.js
@@ -1,5 +1,11 @@
-
-casper.test.begin("Should load app into the document", function(test) {
-    test.assert(true);
+// Should see the page
+casper.test.begin("Should load app into the document", 3, function(test) {
+  casper.start("http://localhost:8080/src/html", function(){
+    test.assertTitleMatches(/Books App/)
+    test.assertExists("#app")
+    test.assertUrlMatches(/src\/html/)
+  })
+  .run(function(){
     test.done();
+  })
 });
